d8094acf1b3f8462f59cb2f97877b1a3e32db669
skyline69 <67526259+skyline69@users.noreply.github.com>
2025-03-23 22:35:09 +0100

    Update docker script & README

--- .linux/bundle.sh
+++ .linux/bundle.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -xe
 
 mkdir -p .linux/bundles
-docker run --rm \
-  -v "$PWD"/.linux/bundles/"$(date --utc +%Y%m%d_%H%M%SZ)":/output \
-  "$(docker buildx build -qf .linux/Dockerfile . --load)" \
-  bash -c 'cp /app/bundles/* /output/'
+output_dir="$PWD/.linux/bundles/$(date --utc +%Y%m%d_%H%M%SZ)"
+
+# Build image and tag it with a temporary name
+docker buildx build -f .linux/Dockerfile . --load -t balatro-mod-manager-temp
+
+# Use the tagged image name
+docker run --rm -v "$output_dir:/output" balatro-mod-manager-temp bash -c 'cp /app/bundles/* /output/'
