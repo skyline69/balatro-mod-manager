294433c80dd7864966c71ef5d692d01bf390246b
janw4ld <janw4ld@tutanota.com>
2025-03-28 00:04:54 +0200

    docker bundle: fix outdated paths, use conventional tag and constant names

--- .hack/linux/bundle.sh
+++ .hack/linux/bundle.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -xe
 
-mkdir -p .linux/bundles
-output_dir="$PWD/.linux/bundles/$(date --utc +%Y%m%d_%H%M%SZ)"
+WORKDIR="$PWD/.hack/linux"
+OUTDIR="${WORKDIR}/bundles/$(date --utc +%Y%m%d_%H%M%SZ)"
+mkdir -p "${OUTDIR}"
 
 # Build image and tag it with a temporary name
-docker buildx build -f .linux/Dockerfile . --load -t balatro-mod-manager-temp
+docker buildx build -f "${WORKDIR}"/Dockerfile . --load -t balatro-mod-manager:temp
 
 # Use the tagged image name
-docker run --rm -v "$output_dir:/output" balatro-mod-manager-temp bash -c 'cp /app/bundles/* /output/'
+docker run --rm -v "${OUTDIR}:/output" balatro-mod-manager:temp bash -c 'cp /app/bundles/* /output/'
